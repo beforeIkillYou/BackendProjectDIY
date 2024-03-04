326260b5215c19f440f6a050ba794c4bd49a582a
beforeIkillYou <simonsimple305@gmail.com>
2024-03-04 21:44:36 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.urlencoded());
 app.use(express.static('public'));
 app.use(cookieParser());
 
-export { app }
+//ROUTES COME HERE BABYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
+
+//importing routes
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
+
+export  { app }
